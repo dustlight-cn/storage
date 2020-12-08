d5edb572d4fb844a8f7cd2af1d5360ab75067eba
Hansin1997 <845612500@qq.com>
2020-12-08 09:20:59 +0800

    Initial commit

---
+++ storage-core/src/main/java/cn/dustlight/storage/core/StorableObject.java
@@ -0,0 +1,16 @@
+package cn.dustlight.storage.core;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public interface StorableObject {
+
+    InputStream getInputStream() throws IOException;
+
+    OutputStream getOutputStream() throws IOException;
+
+    String getKey();
+
+    int getPermission();
+}
---
+++ storage-core/src/main/java/cn/dustlight/storage/local/LocalStorableObject.java
@@ -0,0 +1,56 @@
+package cn.dustlight.storage.local;
+
+import cn.dustlight.storage.core.Permission;
+import cn.dustlight.storage.core.StorableObject;
+
+import java.io.*;
+
+public class LocalStorableObject implements StorableObject {
+
+    protected String path;
+    protected String fileName;
+    protected File file;
+
+    LocalStorableObject(String path, String fileName) {
+        this.path = path;
+        this.fileName = fileName;
+        file = new File(path, fileName);
+    }
+
+    LocalStorableObject(String path, String fileName, int permission) {
+        this.path = path;
+        this.fileName = fileName;
+        file = new File(path, fileName);
+        file.setWritable(Permission.isWritable(permission));
+        file.setReadable(Permission.isReadable(permission));
+    }
+
+    @Override
+    public InputStream getInputStream() throws IOException {
+        return new FileInputStream(file);
+    }
+
+    @Override
+    public OutputStream getOutputStream() throws IOException {
+        return new FileOutputStream(file);
+    }
+
+    @Override
+    public String getKey() {
+        return fileName;
+    }
+
+    @Override
+    public int getPermission() {
+        return Permission.compute(file.canRead(), file.canWrite());
+    }
+
+//    @Override
+//    public Long getContentLength() {
+//        return file.length();
+//    }
+
+    public File getFile() {
+        return file;
+    }
+}
